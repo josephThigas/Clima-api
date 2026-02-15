a54ccd6531539ac1d6130d4a6db3115b4823c051
Thinkpad_will <carlosmaycon443@gmail.com>
2026-02-15 11:56:26 -0300

    habilitando configuração do feign

--- src/main/java/br/com/previsao/clima/Clima_api/ClimaApiApplication.java
+++ src/main/java/br/com/previsao/clima/Clima_api/ClimaApiApplication.java
@@ -6,10 +6,12 @@ import io.swagger.v3.oas.models.info.Info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ClimaApiApplication {
 
     public static void main(String[] args) {
