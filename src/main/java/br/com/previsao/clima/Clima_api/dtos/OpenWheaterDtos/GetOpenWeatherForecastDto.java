07b74b3e584fc906d5436ae7768730eca2495c3e
josephThigas <thiagopombos@hotmail.com>
2026-04-15 14:48:57 -0300

    first commit

---
+++ src/main/java/br/com/previsao/clima/Clima_api/dtos/OpenWheaterDtos/GetOpenWeatherForecastDto.java
@@ -0,0 +1,34 @@
+package br.com.previsao.clima.Clima_api.dtos.OpenWheaterDtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record GetOpenWeatherForecastDto(
+        List<ForecastItemDTO> list,
+        ForecastCityDTO city,
+        String cod
+) {
+
+    public record ForecastItemDTO(
+            @JsonProperty("dt_txt") String dataHora,
+            ForecastMainDTO main,
+            List<ForecastWeatherDTO> weather
+    ) {
+    }
+
+    public record ForecastMainDTO(
+            Double temp
+    ) {
+    }
+
+    public record ForecastWeatherDTO(
+            String description
+    ) {
+    }
+
+    public record ForecastCityDTO(
+            String country
+    ) {
+    }
+}
